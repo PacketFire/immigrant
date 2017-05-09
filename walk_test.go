b3d8877e824bbdb068210da618e5efce0a102c46
Nate Catelli <nate.catelli@isprime.com>
2017-05-08 23:15:16 -0400

    Patch walk to only parse .yml files.

--- walk_test.go
+++ walk_test.go
@@ -5,12 +5,31 @@ import (
 )
 
 const (
-	revisionTestDir string = "test/revisions/"
+	revisionTestDir      string = "test/revisions/"
+	emptyRevisionTestDir string = "test/empty_revisions_dir/"
 )
 
 func TestWalk(t *testing.T) {
 	_, err := Walk(revisionTestDir)
 	if err != nil {
 		t.Fatal(err)
+	}
+}
+
+func TestEmptyWalk(t *testing.T) {
+	_, err := Walk(emptyRevisionTestDir)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestWalkNonYamlFile(t *testing.T) {
+	revisions, err := Walk(revisionTestDir)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if _, p := revisions["revision_txt"]; p == true {
+		t.Fatal("Walk should not parse non yaml files.")
 	}
 }
