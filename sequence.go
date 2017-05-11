40d748e21236c09edad061eab1d2fa18473f2ef2
Nate Catelli <nate.catelli@isprime.com>
2017-05-10 20:57:08 -0400

    WIP: Add ParseSequence prototype

--- sequence.go
+++ sequence.go
@@ -3,3 +3,12 @@ package main
 type Sequence struct {
 	Revisions []*Revision `yaml:"revisions"`
 }
+
+// Parse sequence will take a path to the config directory and attempt to open
+// and parse the file to a sequence. On success a *Sequence and nil are
+// returned. On failure a *Sequence and error are returned.
+func ParseSequence(path string) (*Sequence, error) {
+	seq := new(Sequence)
+
+	return seq, nil
+}
