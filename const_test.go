9b7f8f1158f3463cf6b6054073b9b3c612284da3
Nate Catelli <ncatelli@packetfire.org>
2017-05-11 19:28:43 -0400

    Add config parsing and tests

---
+++ const_test.go
@@ -0,0 +1,9 @@
+package main
+
+const (
+	revisionTestDir          string = "test/parse_test_dir"
+	emptyRevisionTestDir     string = "test/empty_revisions_dir"
+	duplicateRevisionTestDir string = "test/duplicate_revisions"
+	fileExtTestDir           string = "test/file_ext_test"
+	YAMLRevisionsTestDir     string = "test/parse_test_dir_yaml_revisions"
+)
