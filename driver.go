fd407f7084544591ba9882710dd5a498ce9c7a02
Nate Catelli <ncatelli@packetfire.org>
2017-05-11 19:38:23 -0400

    rename DBDriver to Driver

--- driver.go
+++ driver.go
@@ -1,9 +1,9 @@
 package main
 
-// DBDriver defines the necessary methods to interface a database with
+// Driver defines the necessary methods to interface a database with
 // immigrant. State tracking, Migrations and Rollbacks should mostly be
 // defined and implemented by the driver.
-type DBDriver interface {
+type Driver interface {
 	Init(map[string]string)  // Setup connection and state tracking.
 	Migrate(Revision) error  // Execute a migration against the target database.
 	Rollback(Revision) error // Execute a rollback against the target database.
