b1085b3e0d0ef0fbc39c429101b47cd9018bb38c
Nate Catelli <ncatelli@packetfire.org>
2017-05-11 23:04:59 -0400

    Signal and Shutdown handling

--- main.go
+++ main.go
@@ -1,4 +1,46 @@
 package main
 
+import (
+	"flag"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+const (
+	ExitOk  int = 0
+	ExitErr int = 1
+)
+
+var (
+	cd *string // flag for config directory
+)
+
+func init() {
+	cd = flag.String("config-directory",
+		"",
+		"Specify the config directory")
+	flag.Parse()
+}
+
+// Shutdown triggers correct shutdown
+func Shutdown(code int) {
+	os.Exit(code)
+}
+
 func main() {
+	defer func() {
+		if err := recover(); err != nil {
+			Shutdown(ExitErr)
+		}
+	}()
+
+	// Signal Handling
+	sigs := make(chan os.Signal)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		<-sigs
+		Shutdown(ExitOk)
+	}()
+
 }
