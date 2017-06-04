5ffff804398f5038794136a3dbad8b62342c6f2a
Nate Catelli <ncatelli@packetfire.org>
2017-06-04 09:44:30 -0400

    Add DSN struct and Init method to mysql driver.

--- mysql.go
+++ mysql.go
@@ -1,10 +1,38 @@
 package main
 
+import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+)
+
 type MysqlDriver struct {
+	Config DSN
+	Db     *sql.DB
 }
 
-func (this *MysqlDriver) Init(context map[string]string) {
+type DSN struct {
+	user     string
+	pass     string
+	host     string
+	database string
+	params   string
+}
 
+func (this *MysqlDriver) Init(ctx map[string]string) error {
+	this.Config = NewDSN(ctx["username"],
+		ctx["password"],
+		ctx["host"],
+		ctx["database"],
+		ctx["params"])
+
+	db, err := sql.Open("mysql", this.Config.String())
+	if err != nil {
+		return err
+	}
+
+	this.Db = db
+
+	return nil
 }
 
 func (this *MysqlDriver) Migrate(r Revision, c chan error) {
@@ -22,4 +50,18 @@ func (this *MysqlDriver) State() *Revision {
 
 func (This *MysqlDriver) Close() {
 
+}
+
+func NewDSN(user, pass, host, database, params string) DSN {
+	return DSN{
+		user:     user,
+		pass:     pass,
+		host:     host,
+		database: database,
+		params:   params,
+	}
+}
+
+func (this DSN) String() string {
+	return ""
 }
