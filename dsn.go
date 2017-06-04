753d4a286eb33a18a8924d66aa875a00d5e9b38c
Nate Catelli <ncatelli@packetfire.org>
2017-06-04 11:00:50 -0400

    Add docstrings to DSN and mysql driver.

--- dsn.go
+++ dsn.go
@@ -4,15 +4,18 @@ import (
 	"fmt"
 )
 
+// DSN stores connection information used to create a sql.DB object.
 type DSN struct {
-	user     string
-	pass     string
-	host     string
-	proto    string
-	database string
-	params   string
+	user     string // username for connecting to the database.
+	pass     string // password for the connecting user.
+	host     string // hostname of database to connect to.
+	proto    string // protocol for connection ex: tcp
+	database string // database name.
+	params   string // additional parameters.
 }
 
+// NewDSN takes 6 strings representing the values for generating a DSN string.
+// A corresponding DSN object is returned.
 func NewDSN(user, pass, proto, host, database, params string) DSN {
 	if proto == "" {
 		proto = "tcp"
@@ -27,6 +30,7 @@ func NewDSN(user, pass, proto, host, database, params string) DSN {
 	}
 }
 
+// Converts the DSN struct to a DSN string.
 func (this DSN) String() string {
 	dsn := fmt.Sprintf("%s:%s@%s(%s)/%s)",
 		this.user,
--- mysql.go
+++ mysql.go
@@ -5,20 +5,28 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
+const (
+	StateCreate string = `
+CREATE DATABASE immStateMan if not exists;
+use immStateMan;
+CREATE TABLE sequence_tracker (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  revisionID VARCHAR(256),
+  revisionJSON TEXT
+);
+`
+)
+
+// MysqlDriver stores configuration information along with the DB connection
+// management struct.
 type MysqlDriver struct {
 	Config DSN
 	Db     *sql.DB
 }
 
-type DSN struct {
-	user     string
-	pass     string
-	host     string
-	proto    string
-	database string
-	params   string
-}
-
+// Init takes a context with all configuration which it then uses to create the
+// DB connection and bootstrap immigrant's state tracker database. on success
+// nil is returned. On failure, the corresponding errors are returned.
 func (this *MysqlDriver) Init(ctx map[string]string) error {
 	this.Config = NewDSN(ctx["username"],
 		ctx["password"],
@@ -33,23 +41,48 @@ func (this *MysqlDriver) Init(ctx map[string]string) error {
 	}
 
 	this.Db = db
+	if err = db.Ping(); err != nil {
+		return err
+	}
 
 	return nil
 }
 
+// initStateManager attempts to create the state tracker database and tables
+// and is only meant to be called by the Init method. On success, nil is
+// returned. On failure an error is returned.
+func (this *MysqlDriver) initStateManager() error {
+	return nil
+}
+
+// Migrate takes a revision and a channel and attempts to execute all migrate
+// statements defined in the revision object. The channel is primarily used to
+// communicate back to the cli tool whether a migration has completed in case
+// the signal handler is invoked. On success, nil is pushed over the channel.
+// On failure, an error is pushed over the channel.
 func (this *MysqlDriver) Migrate(r Revision, c chan error) {
 
+	c <- nil
 }
 
+// Rollback takes a revision and a channel and attempts to execute all rollback
+// statements defined in the revision object. The channel is primarily used to
+// communicate back to the cli tool whether a migration has completed in case
+// the signal handler is invoked. On success, nil is pushed over the channel.
+// On failure, an error is pushed over the channel.
 func (this *MysqlDriver) Rollback(r Revision, c chan error) {
 
+	c <- nil
 }
 
+// State returns the current revision that the database is at. On success a
+// pointer to a populated Revision is return. On failure, nil is returned.
 func (this *MysqlDriver) State() *Revision {
 
 	return new(Revision)
 }
 
+// Closes the DB object associated with the driver.
 func (this *MysqlDriver) Close() {
 	this.Db.Close()
 }
