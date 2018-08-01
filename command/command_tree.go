e5a789234f9c2f56a63a079cc6378b5d7cdf8093
bramz <brockramz@gmail.com>
2018-08-01 08:00:52 +0200

    Converge cli and command tree registration

--- command/command_tree.go
+++ command/command_tree.go
@@ -1,15 +1,18 @@
 package command
 
 import (
+	"github.com/ncatelli/immigrant/command/converge"
 	"github.com/ncatelli/immigrant/command/version"
 
 	"github.com/mitchellh/cli"
 )
 
 const (
-	cliVersion string = "0.0.1"
+	cliVersion   string = "0.0.1"
+	convergeTest string = "testing"
 )
 
 func init() {
 	Register("version", func(ui cli.Ui) (cli.Command, error) { return version.New(ui, cliVersion), nil })
+	Register("converge", func(ui cli.UI) (cli.Command, error) { return converge.New(ui, convergeTest), nil })
 }
--- command/converge/converge.go
+++ command/converge/converge.go
@@ -2,13 +2,37 @@ package converge
 
 import (
 	"fmt"
+
 	"github.com/mitchellh/cli"
 )
+
+func New(ui cli.Ui, converge string) *cmd {
+	return &cmd{UI: ui, converge: converge}
+}
+
+type cmd struct {
+	UI       cli.Ui
+	converge string
+}
+
+func (c *cmd) Run(_ []string) int {
+	c.UI.Output(fmt.Sprintf("immigrant %s", c.converge))
+
+	return 0
+}
+
+func (c *cmd) Synopsis() string {
+	return "Converges config to db"
+}
+
+func (c *cmd) Help() string {
+	return ""
+}
 
 func ConsumeConfig() {
 
 }
 
 func ApplyConfig() {
-
+	fmt.Println("test")
 }
