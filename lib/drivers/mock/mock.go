8c671d171f9e09d1fda0b9a2a5f50746091667d5
bramz <brockramz@gmail.com>
2018-08-04 12:41:29 +0200

    Build on stubbed out mock

--- lib/drivers/mock/mock.go
+++ lib/drivers/mock/mock.go
@@ -1,12 +1,11 @@
 package mock
 
 import (
-	"github.com/PacketFire/immigrant/lib/core"
+	"github.com/ncatelli/immigrant/lib/core"
 )
 
 type MockDriver struct {
 	Revisions []core.Revision
-	State     core.Revision
 }
 
 func (this *MockDriver) Init(config map[string]string) error {
@@ -14,13 +13,27 @@ func (this *MockDriver) Init(config map[string]string) error {
 }
 
 func Migrate(r core.Revision, ec chan error) {
+	for _, mig := range r.Migrate {
+		if _, err = mig; err != nil {
+			c <- err
+			return
+		}
+	}
 }
 
 func Rollback(r core.Revision, ec chan error) {
+	for _, mig := range r.Rollback {
+		if _, err = mig; err != nil {
+			c <- err
+			return
+		}
+	}
 }
 
-func State() *core.Revision {
+func State(this *MockDriver) *core.Revision {
+	fmt.Println(&core.Revision)
 	return &core.Revision{}
+
 }
 
 func Close() {
