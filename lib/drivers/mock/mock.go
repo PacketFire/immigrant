cf4614c0607aad0746a8ae22d76c7f48ae6ddc00
bramz <brockramz@gmail.com>
2018-08-05 12:46:22 +0200

    Fixed to pass make test

--- lib/drivers/mock/mock.go
+++ lib/drivers/mock/mock.go
@@ -1,6 +1,8 @@
 package mock
 
 import (
+	"errors"
+
 	"github.com/PacketFire/immigrant/lib/core"
 )
 
@@ -12,26 +14,25 @@ func (this *MockDriver) Init(config map[string]string) error {
 	return nil
 }
 
-func (this *MockDriver) Migrate(r core.Revision, ec chan error) {
-	for _, mig := range r.Migrate {
-		if _, err = mig; err != nil {
-			ec <- err
-			return
-		}
+func (this *MockDriver) Migrate(r *core.Revision, ec chan error) {
+	this.Revisions = append(this.Revisions, *r)
+	ec <- nil
+}
+func (this *MockDriver) Rollback(r *core.Revision, ec chan error) {
+	if len(this.Revisions) <= 0 {
+		ec <- errors.New("No revisions applied.")
+		return
 	}
+	this.Revisions = this.Revisions[:len(this.Revisions)-1]
+	ec <- nil
 }
 
-func (this *MockDriver) Rollback(r core.Revision, ec chan error) {
-	for _, mig := range r.Rollback {
-		if _, err = mig; err != nil {
-			ec <- err
-			return
-		}
+func (this *MockDriver) State(r *core.Revision, ec chan error) {
+	if len(this.Revisions) <= 0 {
+		ec <- errors.New("No revisions found.")
 	}
-}
-
-func (this *MockDriver) State() (*core.Revision, error) {
-	return nil, &core.Revision{}
+	this.Revisions = this.Revisions
+	ec <- nil
 }
 
 func Close() {
