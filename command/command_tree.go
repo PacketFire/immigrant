44dc0360742c702e124efeecaee4da07919ba894
bramz <brockramz@gmail.com>
2019-04-13 12:39:41 -0400

    Update command tree

--- command/command_tree.go
+++ command/command_tree.go
@@ -9,10 +9,9 @@ import (
 
 const (
 	cliVersion string = "0.0.1"
-	coutput    string = "converge command"
 )
 
 func init() {
 	Register("version", func(ui cli.Ui) (cli.Command, error) { return version.New(ui, cliVersion), nil })
-	Register("converge", func(ui cli.Ui) (cli.Command, error) { return converge.New(ui, coutput), nil })
+	Register("converge", func(ui cli.Ui) (cli.Command, error) { return converge.New(ui), nil })
 }
