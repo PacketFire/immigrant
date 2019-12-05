337bc373df384f057cc1c0bbccaeb65956033d1c
Nate Catelli <ncatelli@packetfire.org>
2019-12-05 03:34:40 +0000

    Add linting suggestions to version command

--- command/version/version.go
+++ command/version/version.go
@@ -6,25 +6,33 @@ import (
 	"github.com/mitchellh/cli"
 )
 
-func New(ui cli.Ui, version string) *cmd {
-	return &cmd{UI: ui, version: version}
+// New initializes the version command.
+func New(ui cli.Ui, version string) *Cmd {
+	return &Cmd{UI: ui, version: version}
 }
 
-type cmd struct {
+// Cmd represents a command within mitchellh/cli and stores all the context
+// necessary to execute the version command.
+type Cmd struct {
 	UI      cli.Ui
 	version string
 }
 
-func (c *cmd) Run(_ []string) int {
+// Run executes the version command, printing a version string for immigrant.'
+// the return value should always be 0 as this command should never fail.
+func (c *Cmd) Run(_ []string) int {
 	c.UI.Output(fmt.Sprintf("immigrant %s", c.version))
 
 	return 0
 }
 
-func (c *cmd) Synopsis() string {
+// Synopsis returns a short help string for the version command.
+func (c *Cmd) Synopsis() string {
 	return "Prints the immigrant cli version"
 }
 
-func (c *cmd) Help() string {
+// Help returns an empty string as no additional information is needed to
+// execute this command.
+func (c *Cmd) Help() string {
 	return ""
 }
