0cae3babb0e048fb3e86a8253bb160f0552ec6dd
Nate Catelli <ncatelli@packetfire.org>
2019-12-05 02:45:07 +0000

    formatting in revision.go

--- pkg/core/revision.go
+++ pkg/core/revision.go
@@ -4,6 +4,6 @@ package core
 // for a revision name, and a list of both migration and rollback steps.
 type Revision struct {
 	Revision string   `yaml:"revision"` // A unique name for the revision.
-	Migrate  []string `yaml:"migrate"` // A list of migration steps.
+	Migrate  []string `yaml:"migrate"`  // A list of migration steps.
 	Rollback []string `yaml:"rollback"` // A list of rollback steps.
 }
